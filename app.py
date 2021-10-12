338f4c59d299d81ee9402d3eb3ddcebeada3b2fd
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2021-10-12 18:32:36 -0500

    archivo python app.py Flask

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+import smtplib
+
+app = Flask(__name__)
+
+@app.route('/')
+def bienvenido():
+    return render_template('welcome.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('Dashboard.html')
+
+@app.route('/bedrooms')
+def bedrooms():
+    return render_template('bedrooms.html')
