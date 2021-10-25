9ed073043df48bfb06243337c62b6b6e9adc8d58
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2021-10-25 15:41:50 -0500

    Query Consultar base de datos, registro de usuario en base de datos

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from flask import Flask, render_template, request
-import smtplib
+from werkzeug.utils import redirect
+import werkzeug.security as ws
+#import smtplib
 import bd
 
 app = Flask(__name__)
+app.secret_key = 'mi_llave_secreta'
 
 @app.route('/')
 def bienvenido():
-    bd.sql_conexion
     return render_template('welcome.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
+    
     if request.method == 'GET':
         return render_template('login.html')
     else: 
@@ -20,12 +22,13 @@ def login():
             apellido = request.form["apellido"]
             correo = request.form["correo"]
             contraseña = request.form["contraseña"]
-            return redirect('/bedrooms')
+            bd.insertar_usuario(nombre,apellido,correo,ws.generate_password_hash(contraseña))
+            return 'Registro exitoso'
         elif request.form['boton-env'] == "siguiente":
             correoIni = request.form['correo-ini']
             contraIni = request.form["contra-ini"]
-            return redirect('/bedrooms')
-
+            return 'Inicio de sesión exitoso'
+        
 @app.route('/dashboard')
 def dashboard():
     return render_template('Dashboard.html')
@@ -41,4 +44,3 @@ def bedroom_actions():
 @app.route('/bedrooms_qualify')
 def bedroom_qualify():
     return render_template('bedroom-qualify.html')
-
--- bd.py
+++ bd.py
@@ -3,10 +3,34 @@ import sqlite3
 from sqlite3 import Error
 
 
-def sql_connection():
+def obtener_conexion():
     try:
-        con = sqlite3.connect('bdHotelsRaddison.db')
-        return con
+        conexion = sqlite3.connect('DBradHotel.db')
+        return conexion
     except Error: 
         print(Error)
+
+def obtener_registro(tabla, condicion):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
     
+    if condicion == None:
+        strsql = 'SELECT * FROM {}'.format(tabla)
+    else: strsql = 'SELECT * FROM {} WHERE {}'.format(tabla, condicion)
+    
+    cursor.execute(strsql)
+    
+    datos = cursor.fetchall()
+    conexion.close()
+    
+    return datos
+
+def insertar_usuario(nombre, apellido, correo, contraseña):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+    
+    strsql = "INSERT INTO Usuario (nombre, apellido, correo, contraseña) VALUES ('{}','{}','{}','{}')".format(nombre, apellido, correo, contraseña)
+    
+    cursor.execute(strsql)
+    conexion.commit()
+    conexion.close()
