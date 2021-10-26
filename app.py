1c6289355487610b2c65f5882a60ff5b5a9c6d81
Juan Pablo Allin Cañas <jpac.647@gmail.com>
2021-10-25 19:26:44 -0500

    creación vista de usuario, creación de sesiones y uso de funciones hash para alamcenar contraseña en base de datos usando salts

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, jsonify, send_file, session
 from werkzeug.utils import redirect
 import werkzeug.security as ws
-#import smtplib
 import bd
 
 app = Flask(__name__)
 app.secret_key = 'mi_llave_secreta'
+
+@app.before_request
+def antes_peticion():
+    if 'correo' not in session and request.endpoint in ['perfil']:
+       return redirect('/')
+
+    elif 'correo' in session and request.endpoint in ['login']:
+        return redirect('/perfil/{}'.format(session['correo']))
 
 @app.route('/')
 def bienvenido():
@@ -17,18 +24,48 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else: 
+        #Validar los valores de los formularios de registro
         if request.form['boton-env'] == "anterior":
             nombre = request.form['nombre']
             apellido = request.form["apellido"]
             correo = request.form["correo"]
             contraseña = request.form["contraseña"]
             bd.insertar_usuario(nombre,apellido,correo,ws.generate_password_hash(contraseña))
-            return 'Registro exitoso'
+            session['correo'] = correo
+            return redirect('/perfil/{}'.format(correo))
+        #Validar los valores de los formularios de inicio de sesión
         elif request.form['boton-env'] == "siguiente":
             correoIni = request.form['correo-ini']
-            contraIni = request.form["contra-ini"]
-            return 'Inicio de sesión exitoso'
+            registro_usuario = bd.obtener_registro('Usuario', "correo='{}'".format(correoIni))
+            contraseña_bd = registro_usuario[0][4]
+            if registro_usuario is not None:
+                contraIni = request.form["contra-ini"]
+                contraseña_igual = ws.check_password_hash(contraseña_bd, contraIni)
+                if contraseña_igual:
+                    session['correo'] = correoIni
+                    return redirect('/perfil/{}'.format(correoIni))
+            return render_template('login.html')
         
+@app.route('/perfil')
+@app.route('/perfil/<correo>')
+def perfil(correo=None):
+    if correo:
+        registro_usuario = bd.obtener_registro('Usuario', "correo='{}'".format(correo))
+
+        if registro_usuario:
+            id_usuario = registro_usuario[0][0]
+
+            agregar = False
+            if correo == session['correo']:
+                agregar = True
+
+            return render_template('perfil-usuario.html', correo=registro_usuario, agregar = agregar)
+        else:
+            correo = session['correo']
+            return redirect('/perfil/{}'.format(correo))     
+    else:
+        return render_template('perfil-usuario.html')        
+
 @app.route('/dashboard')
 def dashboard():
     return render_template('Dashboard.html')
@@ -44,3 +81,9 @@ def bedroom_actions():
 @app.route('/bedrooms_qualify')
 def bedroom_qualify():
     return render_template('bedroom-qualify.html')
+
+@app.route('/cerrar_sesion')
+def cerrar_sesion():
+    if 'correo' in session:
+        session.pop('correo')
+        return redirect('/')
---
+++ static/js/perfil.js
@@ -0,0 +1,6 @@
+boton_cerrar_sesion = document.getElementById("cerrar_sesion");
+
+boton_cerrar_sesion.addEventListener("click", () => {
+    location.href = "/cerrar_sesion";
+  //CERRAR SESION
+});
